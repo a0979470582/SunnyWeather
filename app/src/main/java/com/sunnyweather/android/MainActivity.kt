baabee63e765b94d3188cf708825672c1a83c9f5
a0979470582 <a0979470582@gmail.com>
2021-03-01 15:21:46 +0800

    Second Commit.

--- app/src/main/java/com/sunnyweather/android/MainActivity.kt
+++ app/src/main/java/com/sunnyweather/android/MainActivity.kt
@@ -2,10 +2,12 @@ package com.sunnyweather.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, "ttt", Toast.LENGTH_SHORT).show()
     }
 }
