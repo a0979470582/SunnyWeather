7f6d360c938063a459bb827ea110e0d2e9081d5c
a0979470582 <a0979470582@gmail.com>
2021-03-03 12:42:34 +0800

    find global city data

--- app/src/main/java/com/sunnyweather/android/MainActivity.kt
+++ app/src/main/java/com/sunnyweather/android/MainActivity.kt
@@ -7,7 +7,8 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_place)
         Toast.makeText(this, "ttt", Toast.LENGTH_SHORT).show()
     }
 }
